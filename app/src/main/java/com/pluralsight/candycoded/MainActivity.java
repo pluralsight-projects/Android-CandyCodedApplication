d801ad9251d54649b85ab953a695b47e01f7363b
BlackBeard-Teach17 <keamogetswemfoloe@gmail.com>
2019-11-01 15:48:51 +0200

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -86,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
         Intent infoIntent = new Intent(this, InfoActivity.class);
         startActivity(infoIntent);
         return super.onOptionsItemSelected(item);
+
     }
     // ***
     // TODO - Task 1 - Show Store Information Activity
