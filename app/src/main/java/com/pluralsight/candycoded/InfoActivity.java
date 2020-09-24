a6e043a9e5712689346cae2872cf645acce8bbb6
Attah Emmanuel <attahemma0@gmail.com>
2020-09-24 15:29:36 +0100

    provided solution for starting Google map with the provided location on the forked Pluralsight Candy Store App

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,13 +1,18 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
 public class InfoActivity extends AppCompatActivity {
+
+    private String mGeoInfo;
+    private static final String GOOGLE_MAPS_PACKAGE = "com.google.android.apps.maps";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,15 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
+    public void createMapIntent(View view){
+        mGeoInfo = "geo:0,0?q=618 E South St Orlando, FL 32801";
+        Uri uUri = Uri.parse(mGeoInfo);
+
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW,uUri).setPackage(GOOGLE_MAPS_PACKAGE);
+        if(mapIntent.resolveActivity(getPackageManager()) != null){
+            startActivity(mapIntent);
+        }
+    }
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
