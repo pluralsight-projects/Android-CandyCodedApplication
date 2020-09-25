d18ccfafb4445474d21bc9c8cff7901dd3b2ee1f
Blexn <Onyeka_blessing@yahoo.com>
2020-09-25 22:29:21 +0100

    candy store app

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,12 +1,13 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
-
 public class InfoActivity extends AppCompatActivity {
 
     @Override
@@ -21,11 +22,22 @@ public class InfoActivity extends AppCompatActivity {
                 into(candyStoreImageView);
 
 
+
     }
 
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
+
+    public void createMapIntent(View view) {
+        Uri uriAddress = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+        Intent mapIntent =new Intent (Intent.ACTION_VIEW, uriAddress);
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        if (mapIntent.resolveActivity(getPackageManager()) != null){
+            startActivity(mapIntent);
+        }
+    }
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
