ccf569b9d3c6dabfd265a1667db0e7bcd9adcb1d
ekemainai1 <santacbax@gmail.com>
2020-10-11 21:10:50 +0100

    Todo cleared

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -33,13 +33,6 @@ public class InfoActivity extends AppCompatActivity {
 
     }
 
-    // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
-
-    // ***
-    // TODO - Task 3 - Launch the Phone Activity
-    // ***
 
     public void createMapIntent(View view){
         Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
