26aa41e6744b75c92551fc7cce74af5c2ab052da
scarnation <nnadiukwuoscar@gmail.com>
2020-10-14 23:17:02 +0100

    adde the intent name

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -26,8 +26,8 @@ public class InfoActivity extends AppCompatActivity {
     }
     public void createMapIntent(View view) {
         Uri jaga = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, jaga)
-            .setPackage("com.google.android.apps.maps");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, jaga);
+        mapIntent.setPackage("com.google.android.apps.maps");
         if (mapIntent.resolveActivity(getPackageManager()) != null){
             startActivity(mapIntent);
 
