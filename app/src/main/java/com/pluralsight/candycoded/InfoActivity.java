84a52aa899a7133647a6c85a89018e9285a224aa
scarnation <nnadiukwuoscar@gmail.com>
2020-10-14 22:37:17 +0100

    red lights

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,8 +1,10 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -19,6 +21,18 @@ public class InfoActivity extends AppCompatActivity {
         Picasso.with(this).
                 load(uri).
                 into(candyStoreImageView);
+        public void createMapIntent(View view){
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW);
+            mapIntent.setData(Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801"));
+            mapIntent.setPackage("geo:0,0?q=618 E South St Orlando, FL 32801")
+            if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                startActivity(mapIntent);
+            }
+
+
+
+
+        }
 
 
     }
