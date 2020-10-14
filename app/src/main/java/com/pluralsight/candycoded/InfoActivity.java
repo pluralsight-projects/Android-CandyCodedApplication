cba8c00087ee815cc8aab1204380e71f8692b3ca
scarnation <nnadiukwuoscar@gmail.com>
2020-10-14 23:01:08 +0100

    what is the wahala

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -28,11 +28,11 @@ public class InfoActivity extends AppCompatActivity {
         Uri jaga = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, jaga)
             .setPackage("com.google.android.apps.maps");
-        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+        if (mapIntent.resolveActivity(getPackageManager()) != null){
             startActivity(mapIntent);
+
         }
-
-
+        
 
 
     }
